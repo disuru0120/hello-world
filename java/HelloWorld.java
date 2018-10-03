c72edc772e626ad7c5715c7af8a4cebec7084ccc
disuru0120 <43803674+disuru0120@users.noreply.github.com>
2018-10-03 11:29:29 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -4,5 +4,7 @@ import java.io.*;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World and Sri Lanka");
+
     }
 }
